7b84ad229ec42a921cdec6e8c6ba66fd21e9c93e
zeyad0z <zeyadabdelaziz691@gmail.com>
2025-09-08 11:17:22 +0300

    BaseCardUpdates

---
+++ app/components/UI/BaseCard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="w-full max-w-md sm:max-w-lg lg:max-w-[29rem] shadow-black/25 shadow-2xl p-6 sm:p-8 rounded-lg mt-10 sm:mt-16 lg:mt-0 bg-gray-50"
+  >
+    <slot />
+  </div>
+</template>
