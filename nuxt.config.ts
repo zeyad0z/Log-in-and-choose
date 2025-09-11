c6e0895cc7b8d0d176ed33531bacc475f3951917
zeyad0z <zeyadabdelaziz691@gmail.com>
2025-09-11 16:25:23 +0300

    last update

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui", "@nuxt/icon", "@pinia/nuxt"],
+  css: ["@/assets/css/main.css"],
+  app: {
+    baseURL: "/Log-in-and-choose/",
+  },
+  nitro: {
+    preset: "github-pages",
+  },
+
+  colorMode: {
+    preference: "light",
+    fallback: "light",
+    classSuffix: "",
+    storageKey: "nuxt-color-mode",
+  },
+});
